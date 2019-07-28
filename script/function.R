4947d7890a58357800653aba310cf45211889bfc
takuto0831 <takut0.mirage@gmail.com>
2019-07-28 17:04:43 +0900

    update

--- script/function.R
+++ script/function.R
@@ -1,6 +1,12 @@
 library(tidyverse)
-
+library(skimr)
 view_data <- function(data){
-  return(glimpse(data))
+  print("use skim function");  skim(data) %>% kable
+  print("use glimpse function"); glimpse(data)
+}
+
+shape_data <- function(data){
+  sprintf("データは%d行, %d列です!!",
+          NROW(data), NCOL(data))
 }
 
